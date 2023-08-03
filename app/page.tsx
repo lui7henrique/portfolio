4f48ea71027920970a24c7b3bc7c48aa5104f14c
Luiz Henrique <7henrique18@gmail.com>
2023-08-03 19:39:59 -0300

    fix: Home hero section on mobile devices

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,13 @@ export default async function Home() {
   return (
     <>
       <main className="max-w-screen-lg mx-auto px-4">
-        <section className=" py-32 flex flex-col justify-center border-b">
-          <div className="flex items-end gap-4">
-            <h2 className="text-7xl font-bold text-zinc-900">Hello,</h2>
+        <section className="py-32 flex flex-col justify-center items-center border-b select-none">
+          <h2 className="text-6xl font-bold text-zinc-900 ">
+            Hello! I&apos;m Luiz Henrique <span>🖐</span>
+          </h2>
 
-            <h2 className="font-bold text-zinc-900 lg:whitespace-normal xl:whitespace-nowrap text-4xl xl:text-7xl">
-              I&apos;m Luiz Henrique 🖐
-            </h2>
-          </div>
-
-          <h2 className="text-4xl font-regular text-zinc-700">
-            I&apos;m a software engineer currently based in Brazil.
+          <h2 className="text-xl lg:text-3xl font-regular text-zinc-700 mt-4">
+            A software engineer currently based in Brazil.
           </h2>
         </section>
 
