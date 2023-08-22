8f82148e1f7740fb5b769a8cfda9c10f92efdc35
Luiz Henrique <7henrique18@gmail.com>
2023-08-22 12:56:57 -0300

    fix: Image's sizes

--- components/CompanyIcon/index.tsx
+++ components/CompanyIcon/index.tsx
@@ -11,12 +11,12 @@ export const CompanyIcon = (props: CompanyIconProps) => {
 
   return (
     <figure
-      className={"w-10 h-10 relative rounded-xl lg:w-12 lg:h-12"}
+      className={"w-10 h-10 relative rounded-xl lg:w-12 lg:h-12 flex items-center justify-center"}
       style={{
         backgroundColor: companyColor.hex,
       }}
     >
-      <Image src={companyLogo.url} alt={companyName} className="p-2" fill sizes="(max-width: 768px) 40px, (max-width: 1200px) 48px"
+      <Image src={companyLogo.url} alt={companyName} className="p-2" width={48} height={48}
       />
     </figure>
   );
